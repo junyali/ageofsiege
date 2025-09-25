2f7bfabb53e0dd14c2ad12ac1b8c4d43b5ef2e67
Junya <junjia.liu@pm.me>
2025-09-25 19:39:42 +0100

    Register events

---
+++ src/main/java/io/github/junyali/ageofsiege/event/AgeofSiegeEvents.java
@@ -0,0 +1,9 @@
+package io.github.junyali.ageofsiege.event;
+
+import io.github.junyali.ageofsiege.AgeofSiege;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(modid = AgeofSiege.MODID)
+public class AgeofSiegeEvents {
+
+}
