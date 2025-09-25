8cb72015af1a08196b51c4db14f01a8130fe5351
Junya <junjia.liu@pm.me>
2025-09-26 00:49:42 +0100

    Add barrel functionalities?

--- src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
+++ src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
@@ -1,46 +1,56 @@
 package io.github.junyali.ageofsiege.block.custom;
 
-import com.mojang.serialization.MapCodec;
 import io.github.junyali.ageofsiege.block.entity.BaseCrateBlockEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class BaseCrateBlock extends BaseEntityBlock {
+	public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
 	public BaseCrateBlock(BlockBehaviour.Properties properties) {
 		super(properties);
-	}
-
-	public abstract int getInventorySize();
-
-	@Override
-	protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
-		throw new UnsupportedOperationException("Not implemented");
+		this.registerDefaultState(this.stateDefinition.any().setValue(OPEN, false));
 	}
 
 	@Override
-	protected @NotNull RenderShape getRenderShape(@NotNull BlockState blockState) {
-		return RenderShape.MODEL;
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(OPEN);
 	}
 
 	@Override
-	protected @NotNull InteractionResult use(@NotNull BlockState blockState, @NotNull Level level, @NotNull BlockPos blockPos,
-	                                         @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hitResult) {
-		if (!level.isClientSide) {
-			if (level.getBlockEntity(blockPos) instanceof BaseCrateBlockEntity baseCrateBlockEntity) {
-				openMenu(player, baseCrateBlockEntity);
+	protected @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos blockPos,
+	                                         @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+		if (level.isClientSide) {
+			return InteractionResult.SUCCESS;
+		}
+
+		BlockEntity blockEntity = level.getBlockEntity(blockPos);
+		if (blockEntity instanceof BaseCrateBlockEntity crateEntity) {
+			player.openMenu((MenuProvider) crateEntity);
+		}
+		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	public void onRemove(@NotNull BlockState blockState, @NotNull Level level, @NotNull BlockPos blockPos, @NotNull BlockState newBlockState, boolean isMoving) {
+		if (!blockState.is(newBlockState.getBlock())) {
+			BlockEntity blockEntity = level.getBlockEntity(blockPos);
+			if (blockEntity instanceof BaseCrateBlockEntity crateEntity) {
+				Containers.dropContents(level, blockPos, (Container) crateEntity);
+				level.updateNeighbourForOutputSignal(blockPos, this);
 			}
 		}
-		return InteractionResult.sidedSuccess(level.isClientSide);
+		super.onRemove(blockState, level, blockPos, newBlockState, isMoving);
 	}
-
-	protected abstract void openMenu(Player player, BaseCrateBlockEntity baseCrateBlockEntity);
 }
