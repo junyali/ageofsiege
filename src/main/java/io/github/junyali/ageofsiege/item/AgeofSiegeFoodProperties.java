6d4813dd26737a21f1c73cded971d7fcc93bea8d
Junya <junjia.liu@pm.me>
2025-09-25 20:21:09 +0100

    Add item model and translations for newly added items

--- src/main/java/io/github/junyali/ageofsiege/item/AgeofSiegeFoodProperties.java
+++ src/main/java/io/github/junyali/ageofsiege/item/AgeofSiegeFoodProperties.java
@@ -3,6 +3,6 @@ package io.github.junyali.ageofsiege.item;
 import net.minecraft.world.food.FoodProperties;
 
 public class AgeofSiegeFoodProperties {
-	public static final FoodProperties HARDTACK_BREAD = new FoodProperties.Builder().nutrition(3).saturationModifier(6.0f)
+	public static final FoodProperties HARDTACK_BREAD = new FoodProperties.Builder().nutrition(3).saturationModifier(0.6f)
 			.build();
 }
