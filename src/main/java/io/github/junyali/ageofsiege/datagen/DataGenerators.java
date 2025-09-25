28b743c7acdde910c928d41c90a1367c81b869a5
Junya <junjia.liu@pm.me>
2025-09-25 17:09:00 +0100

    Setup datagen package

---
+++ src/main/java/io/github/junyali/ageofsiege/datagen/DataGenerators.java
@@ -0,0 +1,25 @@
+package io.github.junyali.ageofsiege.datagen;
+
+import io.github.junyali.ageofsiege.AgeofSiege;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber(modid = AgeofSiege.MODID)
+public class DataGenerators {
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event) {
+		DataGenerator generator = event.getGenerator();
+		PackOutput packOutput = generator.getPackOutput();
+		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+		// providers go here yada yada
+	}
+}
