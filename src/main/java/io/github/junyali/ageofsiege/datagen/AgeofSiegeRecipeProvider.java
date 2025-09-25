bf7a9ea0aca27c9fdb9e4c3180c329466428281e
Junya <junjia.liu@pm.me>
2025-09-25 20:57:12 +0100

    Add crafting recipes for newly added consumables

--- src/main/java/io/github/junyali/ageofsiege/datagen/AgeofSiegeRecipeProvider.java
+++ src/main/java/io/github/junyali/ageofsiege/datagen/AgeofSiegeRecipeProvider.java
@@ -1,9 +1,13 @@
 package io.github.junyali.ageofsiege.datagen;
 
+import io.github.junyali.ageofsiege.item.AgeofSiegeItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,5 +21,20 @@ public class AgeofSiegeRecipeProvider extends RecipeProvider implements IConditi
 	@Override
 	protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
 		// crafting / smelting recipes go here
+		ShapedRecipeBuilder.shaped(RecipeCategory.FOOD, AgeofSiegeItems.HARDTACK_BREAD.get(), 3)
+				.pattern(" @ ")
+				.pattern("###")
+				.pattern("   ")
+				.define('#', Items.WHEAT)
+				.define('@', Items.SAND)
+				.unlockedBy("has_wheat", has(Items.WHEAT)).save(recipeOutput);
+
+		ShapedRecipeBuilder.shaped(RecipeCategory.FOOD, AgeofSiegeItems.BLACK_BREAD.get(), 3)
+				.pattern(" @ ")
+				.pattern("###")
+				.pattern("   ")
+				.define('#', Items.WHEAT)
+				.define('@', Items.CHARCOAL)
+				.unlockedBy("has_wheat", has(Items.WHEAT)).save(recipeOutput);
 	}
 }
