26cf1b5d5c11b33a72b7a396d0d3989fa137a95f
Junya <junjia.liu@pm.me>
2025-09-25 19:49:22 +0100

    Add custom mead potion

--- src/main/java/io/github/junyali/ageofsiege/event/AgeofSiegeEvents.java
+++ src/main/java/io/github/junyali/ageofsiege/event/AgeofSiegeEvents.java
@@ -1,9 +1,20 @@
 package io.github.junyali.ageofsiege.event;
 
 import io.github.junyali.ageofsiege.AgeofSiege;
+import io.github.junyali.ageofsiege.potion.AgeofSiegePotions;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionBrewing;
+import net.minecraft.world.item.alchemy.Potions;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
 
 @EventBusSubscriber(modid = AgeofSiege.MODID)
 public class AgeofSiegeEvents {
+	@SubscribeEvent
+	public static void onBrewingRecipeRegister(RegisterBrewingRecipesEvent event) {
+		PotionBrewing.Builder builder = event.getBuilder();
 
+		builder.addMix(Potions.AWKWARD, Items.HONEYCOMB, AgeofSiegePotions.MEAD_POTION);
+	}
 }
--- src/main/java/io/github/junyali/ageofsiege/potion/AgeofSiegePotions.java
+++ src/main/java/io/github/junyali/ageofsiege/potion/AgeofSiegePotions.java
@@ -1,13 +1,22 @@
 package io.github.junyali.ageofsiege.potion;
 
 import io.github.junyali.ageofsiege.AgeofSiege;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.alchemy.Potion;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class AgeofSiegePotions {
 	public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(BuiltInRegistries.POTION, AgeofSiege.MODID);
+
+	public static final Holder<Potion> MEAD_POTION = POTIONS.register("mead_potion",
+			() -> new Potion(
+					new MobEffectInstance(MobEffects.DAMAGE_BOOST, 2400, 0),
+					new MobEffectInstance(MobEffects.CONFUSION, 7200, 0)
+			));
 
 	public static void register(IEventBus eventBus) {
 		POTIONS.register(eventBus);
