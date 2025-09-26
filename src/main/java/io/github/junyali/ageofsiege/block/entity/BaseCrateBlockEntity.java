7ac8589a40fc58da56cb1d50562fae62d99fa3d7
Junya <junjia.liu@pm.me>
2025-09-26 01:19:08 +0100

    Override methods for crate entity

--- src/main/java/io/github/junyali/ageofsiege/block/entity/BaseCrateBlockEntity.java
+++ src/main/java/io/github/junyali/ageofsiege/block/entity/BaseCrateBlockEntity.java
@@ -2,22 +2,79 @@ package io.github.junyali.ageofsiege.block.entity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class BaseCrateBlockEntity extends BlockEntity {
+public abstract class BaseCrateBlockEntity extends BlockEntity implements Container, MenuProvider {
+	protected NonNullList<ItemStack> items;
+	public abstract int getInventorySize();
+
 	public BaseCrateBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
 		super(type, blockPos, blockState);
+		this.items = NonNullList.withSize(getInventorySize(), ItemStack.EMPTY);
 	}
 
-	public abstract int getInventorySize();
-	public abstract void clearContents();
+	@Override
+	public int getContainerSize() {
+		return getInventorySize();
+	}
+
+	@Override
+	public @NotNull ItemStack getItem(int slot) {
+		return items.get(slot);
+	}
+
+	@Override
+	public @NotNull ItemStack removeItem(int slot, int amount) {
+		ItemStack result = ContainerHelper.removeItem(items, slot, amount);
+		if (!result.isEmpty()) {
+			setChanged();
+		}
+		return result;
+	}
+
+	@Override
+	public @NotNull ItemStack removeItemNoUpdate(int slot) {
+		return ContainerHelper.takeItem(items, slot);
+	}
+
+	@Override
+	public void setItem(int slot, @NotNull ItemStack stack) {
+		items.set(slot, stack);
+		if (stack.getCount() > getMaxStackSize()) {
+			stack.setCount(getMaxStackSize());
+		}
+		setChanged();
+	}
+
+	@Override
+	public void clearContent() {
+		items.clear();
+	}
+
+	@Override
+	protected void saveAdditional(@NotNull CompoundTag tag, HolderLookup.@NotNull Provider registries) {
+		super.saveAdditional(tag, registries);
+		ContainerHelper.saveAllItems(tag, items, registries);
+	}
+
+	@Override
+	protected void loadAdditional(@NotNull CompoundTag tag, HolderLookup.@NotNull Provider registries) {
+		super.loadAdditional(tag, registries);
+		items = NonNullList.withSize(getInventorySize(), ItemStack.EMPTY);
+		ContainerHelper.loadAllItems(tag, items, registries);
+	}
 
 	@Override
 	public Packet<ClientGamePacketListener> getUpdatePacket() {
