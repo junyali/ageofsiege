6f5e5d34470b40bc7920c276ebe90ac3809ccfba
Junya <junjia.liu@pm.me>
2025-09-26 01:11:38 +0100

    Implement the correct methods :skull:

--- src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
+++ src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
@@ -5,8 +5,10 @@ import io.github.junyali.ageofsiege.block.entity.BaseCrateBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
@@ -22,6 +24,8 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class BaseCrateBlock extends BaseEntityBlock {
 	public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
+	public abstract int getInventorySize();
+
 	public BaseCrateBlock(BlockBehaviour.Properties properties) {
 		super(properties);
 		this.registerDefaultState(this.stateDefinition.any().setValue(OPEN, false));
@@ -33,15 +37,20 @@ public abstract class BaseCrateBlock extends BaseEntityBlock {
 	}
 
 	@Override
-	protected @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos blockPos,
-	                                         @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+	protected @NotNull ItemInteractionResult useItemOn(@NotNull ItemStack stack, @NotNull BlockState blockState, @NotNull Level level, @NotNull BlockPos blockPos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+		return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+	}
+
+	@Override
+	protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos blockPos, @NotNull Player player, @NotNull BlockHitResult hit) {
 		if (level.isClientSide) {
 			return InteractionResult.SUCCESS;
 		}
 
 		BlockEntity blockEntity = level.getBlockEntity(blockPos);
 		if (blockEntity instanceof BaseCrateBlockEntity crateEntity) {
-			player.openMenu((MenuProvider) crateEntity);
+			player.openMenu(crateEntity);
+			return InteractionResult.CONSUME;
 		}
 		return InteractionResult.SUCCESS;
 	}
