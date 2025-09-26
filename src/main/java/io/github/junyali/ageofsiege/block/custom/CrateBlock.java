e3e79c618e789691c4d61a441415c39fe8179ffc
Junya <junjia.liu@pm.me>
2025-09-26 15:05:31 +0100

    Rewrite crate class as a normal class instead of a parent class

--- src/main/java/io/github/junyali/ageofsiege/block/custom/CrateBlock.java
+++ src/main/java/io/github/junyali/ageofsiege/block/custom/CrateBlock.java
@@ -1,7 +1,9 @@
 package io.github.junyali.ageofsiege.block.custom;
 
+import com.mojang.serialization.MapCodec;
 import io.github.junyali.ageofsiege.block.entity.BaseCrateBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
@@ -10,27 +12,46 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class BaseCrateBlock extends BaseEntityBlock {
-	public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
-	public abstract int getInventorySize();
+import java.util.List;
 
-	public BaseCrateBlock(BlockBehaviour.Properties properties) {
+public class CrateBlock extends BaseEntityBlock {
+	public static final MapCodec<CrateBlock> CODEC = simpleCodec(CrateBlock::new);
+	public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
+
+	public CrateBlock(Properties properties) {
 		super(properties);
 		this.registerDefaultState(this.stateDefinition.any().setValue(OPEN, false));
 	}
 
 	@Override
+	protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
+		return CODEC;
+	}
+
+	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(OPEN);
+	}
+
+	@Override
+	protected @NotNull RenderShape getRenderShape(@NotNull BlockState blockState) {
+		return RenderShape.MODEL;
+	}
+
+	@Override
+	public BlockEntity newBlockEntity(@NotNull BlockPos blockPos, @NotNull BlockState blockState) {
+		return new BaseCrateBlockEntity(blockPos, blockState);
 	}
 
 	@Override
@@ -50,5 +71,31 @@ public abstract class BaseCrateBlock extends BaseEntityBlock {
 			return InteractionResult.CONSUME;
 		}
 		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	protected void onRemove(@NotNull BlockState blockState, @NotNull Level level, @NotNull BlockPos blockPos, @NotNull BlockState newBlockState, boolean isMoved) {
+		if (!blockState.is(newBlockState.getBlock())) {
+			BlockEntity blockEntity = level.getBlockEntity(blockPos);
+			if (blockEntity instanceof BaseCrateBlockEntity crateEntity) {
+				level.updateNeighbourForOutputSignal(blockPos, this);
+			}
+		}
+		super.onRemove(blockState, level, blockPos, newBlockState, isMoved);
+	}
+
+	@Override
+	protected @NotNull List<ItemStack> getDrops(@NotNull BlockState blockState, LootParams.@NotNull Builder lootParams) {
+		List<ItemStack> drops = super.getDrops(blockState, lootParams);
+
+		BlockEntity blockEntity = lootParams.getOptionalParameter(LootContextParams.BLOCK_ENTITY);
+		if (blockEntity instanceof BaseCrateBlockEntity crateEntity) {
+			ItemStack crateItem = new ItemStack(this);
+
+			CompoundTag nbt = new CompoundTag();
+			// store inventory code goes here l8r when i refactor the entity class
+		}
+
+		return drops;
 	}
 }
