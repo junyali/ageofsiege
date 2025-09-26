0c7b1f1a5caa09935289f7eb9a77fb4308dea4ab
Junya <junjia.liu@pm.me>
2025-09-26 01:22:33 +0100

    Remove unnecessary imports

--- src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
+++ src/main/java/io/github/junyali/ageofsiege/block/custom/BaseCrateBlock.java
@@ -1,18 +1,15 @@
 package io.github.junyali.ageofsiege.block.custom;
 
-import com.mojang.serialization.MapCodec;
 import io.github.junyali.ageofsiege.block.entity.BaseCrateBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
